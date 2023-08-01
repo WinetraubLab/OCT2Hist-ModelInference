40b677b11e8ac2b87cec71eecc9c867189f5606b
Danny Barash <dannybarash@DN2tevk8.SUNet>
2023-08-01 14:03:59 -0700

    black out under the tissue

--- utils/masking_utils.py
+++ utils/masking_utils.py
@@ -14,70 +14,78 @@ def get_first_zero_and_next_non_zero_idx(arr):
   next_non_zero = (tmp>0).argmax(axis=0)
   return first_zero, next_non_zero
 
-def mask_image(oct_input_image_path):
-  # a new image from
-  path_new_image = oct_input_image_path
-  img = readImgByPath(path_new_image)
-
+def mask_image(img):
   filt_img = smooth(img)
-
-  filt_below_min_signal(filt_img)
-
-  # height, mid = blackout_out_of_tissue_gel(filt_img, img)
-  img, extra_mid = blackout_out_of_tissue_gel(filt_img, img)
-  #
-  # top_row = max(mid - 128, 0)
-  # bottom_row = min(mid + 128, height)
-  # cropped_img = img[top_row:bottom_row, :, :]
+  min_signal = find_min_signal(filt_img)
+  filt_img[filt_img < min_signal] = 0
+  img = blackout_out_of_tissue_gel(filt_img, img, min_signal)
+  return img
 
 
-  return img, extra_mid
+def find_the_longest_non_zero_row(m_mean_arr):
+  current_count = 0
+  longest_count = 0
+  longest_count_start = -1
+  longest_count_end = -1
+  current_start = 0
+  current_end = 0
+
+  for i in range(len(m_mean_arr)):
+    if m_mean_arr[i]>0:
+      if current_count == 0:
+        current_start = i
+      current_count += 1
+
+    if m_mean_arr[i]==0 and current_count > 0:
+      current_end = i
+      if current_count > longest_count:
+        longest_count_start = current_start
+        longest_count_end = current_end
+        longest_count = current_count
+        current_count = 0
+
+  return longest_count_start, longest_count_end
 
 
-def blackout_out_of_tissue_gel(filt_img, img):
+
+def blackout_out_of_tissue_gel(filt_img, img, min_signal, top_bottom_10percent_assumption = False):
+  if top_bottom_10percent_assumption:
+    blackout_10percent(filt_img)
+  # get mean over x axis (rows) to get one value for each depth, for new thresholded image.
+  m_mean = np.nanmean(filt_img, axis=1)
+  m_mean_arr = np.copy(m_mean[:, 0])
+  begin,end = find_the_longest_non_zero_row(m_mean_arr)
+  mid = int((begin+end)/2)
+  #remove any line which is not part of the longest non zero sequence
+  # filt_img[:start] = 0
+  #apply filter on image, for the lower half (row > end), and below signal (filt_img == 0).
+  filt_img[:mid,:,:] = 1
+  img[(filt_img == 0)] = 0
+  return img
+
+
+def blackout_10percent(filt_img):
   # Assuming the top and bottom 10% of the mask should be black:
   height = filt_img.shape[0]
   p = 0.1
   filt_img[0:int(p * height), :] = 0
   filt_img[int((1 - p) * height):, :] = 0
-  # get mean over x axis (rows) to get one value for each depth, for new thresholded image.
-  m_mean = np.nanmean(filt_img, axis=1);
-  # assume there's a non-black area around the middle of the image, surrounded by black area.
-  # Find the first black line, and the next non black line.
-  m_mean_arr = np.copy(m_mean[:, 0])
-  first_zero, next_non_zero = get_first_zero_and_next_non_zero_idx(m_mean_arr);
-  # Do the same to the vertically flipped (mirrored around the x axis) image.
-  flipped = np.copy(np.flip(m_mean_arr))
-  first_zero_from_the_end, next_non_zero_from_the_end = get_first_zero_and_next_non_zero_idx(flipped);
-  h = len(flipped)
-  first_zero_from_the_end = h - first_zero_from_the_end
-  next_non_zero_from_the_end = h - next_non_zero_from_the_end
-  # "Blackout" everything above and below the tissue-gel interface
-  margin = 10;  # margin around the high-enough-snr area.
-  top = next_non_zero - margin;
-  bottom = next_non_zero_from_the_end + margin;
-  img[:top, :] = 0
-  img[bottom:, :] = 0
-  mid = int((bottom + top) / 2.0);
-  filt_img[:mid, :] = 1;
-  img[filt_img == 0] = 0;
-  return img,mid
 
 
-def filt_below_min_signal(filt_img):
+def find_min_signal(filt_img):
   m_mean_max, m_mean_min = get_rows_min_max(filt_img)
   # Finally we define a threshold for OCT intensity, anything below that will be blacked out
-  minSignal = 0.28 * (m_mean_max - m_mean_min) + m_mean_min;
-  filt_img[filt_img < minSignal] = 0;
+  minSignal = 0.28 * (m_mean_max - m_mean_min) + m_mean_min
+  return minSignal
 
 
 def get_rows_min_max(filt_img):
   # Average over x axis (rows) to get one value for each depth
-  m_mean = np.nanmean(filt_img, axis=1)
+  m_mean = np.nanmean(filt_img[:,:,0], axis=1)
   # Then we figure out what is the "brightest" row by taking percentile:
   m_mean_max = np.percentile(m_mean, 99, axis=0)
   # Then we figure out what is the noise floor of the device, by examining the bottom 50 rows of OCT image
-  m_mean_min = np.mean(m_mean[-50:]);
+  m_mean_min = np.mean(m_mean[-50:])
   return m_mean_max, m_mean_min
 
 
@@ -88,3 +96,9 @@ def smooth(img):
   filter_size = int(2 * np.ceil(2 * sigma) + 1)
   filt_img = cv2.GaussianBlur(img, (filter_size, filter_size), sigma)
   return filt_img
+
+if __name__ == '__main__':
+  oct_input_image_path = "/Users/dannybarash/Code/oct/OCT2Hist-UseModel/emilie_image.png"
+  oct_image_orig = cv2.imread(oct_input_image_path)
+  oct_image_orig = cv2.cvtColor(oct_image_orig, cv2.COLOR_BGR2RGB)
+  masked_image = mask_image(oct_image_orig)
