e7e9547b89773b252f9b2196c86f917a12cfdf73
MyYo <MyYo@users.noreply.github.com>
2023-08-04 15:28:09 -0700

    Typo

--- utils/run_pix2pix_utils.py
+++ utils/run_pix2pix_utils.py
@@ -12,27 +12,27 @@ base_folder = "/content/OCT2Hist-UseModel/pytorch-CycleGAN-and-pix2pix"
 # Folder containing the pre-trained model
 model_folder = "/content/drive/Shareddrives/Yolab - Current Projects/_Datasets/2020-11-10 10x Model (Paper V2)"
 
+# Private module to wrapp around using cmd
+def _run_subprocess(cmd):
+    result = subprocess.run([cmd], capture_output=True, text=True, shell=True)
+    if result.returncode != 0:
+        print(f"{cmd}")
+        print(f"Command failed with exit code {result.returncode}.")
+        print("Error message:")
+        print(result.stderr)
+        raise RuntimeError("See error")
 
 def setup_network():
     """ Set up environment load the network parameters, run this code once """
     
-    def run_subprocess(cmd):
-        result = subprocess.run([cmd], capture_output=True, text=True, shell=True)
-        if result.returncode != 0:
-            print(f"{cmd}")
-            print(f"Command failed with exit code {result.returncode}.")
-            print("Error message:")
-            print(result.stderr)
-            raise RuntimeError("See error")
-        
     # Install dependencies
-    run_subprocess(f'pip install -r {base_folder}/requirements.txt')
+    _run_subprocess(f'pip install -r {base_folder}/requirements.txt')
     
     # Mount google drive (if not already mounted)
     drive.mount('/content/drive/')
 
     # Copy model parameters to the correct location
-    run_subprocess(f'mkdir {base_folder}/checkpoints')
-    run_subprocess(f'mkdir {base_folder}/checkpoints/pix2pix/')
-    run_subprocess(f'cp "{model_folder}/latest_net_G.pth" {base_folder}/checkpoints/pix2pix/')
-    run_subprocess(f'cp "{model_folder}/latest_net_D.pth" {base_folder}/checkpoints/pix2pix/')
+    _run_subprocess(f'mkdir {base_folder}/checkpoints')
+    _run_subprocess(f'mkdir {base_folder}/checkpoints/pix2pix/')
+    _run_subprocess(f'cp "{model_folder}/latest_net_G.pth" {base_folder}/checkpoints/pix2pix/')
+    _run_subprocess(f'cp "{model_folder}/latest_net_D.pth" {base_folder}/checkpoints/pix2pix/')
