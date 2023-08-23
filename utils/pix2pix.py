dbd0f58e5a73399eb543ea4daf86091e3f2057c7
MyYo <MyYo@users.noreply.github.com>
2023-08-22 19:46:02 -0700

    Lift evaluating pix2pix network to the wrapper pix2pix.py

--- utils/pix2pix.py
+++ utils/pix2pix.py
@@ -1,6 +1,8 @@
 # This util is a wrapper to the pix2pix algorithm and allows
 # us to evaluate the model by providing an input image
 
+import cv2
+import numpy
 import os
 import subprocess
 from google.colab import drive
@@ -35,4 +37,35 @@ def setup_network(
     # Copy model parameters to the correct location
     _run_subprocess(f'mkdir {base_folder}/checkpoints')
     _run_subprocess(f'mkdir {base_folder}/checkpoints/pix2pix/')
-    _run_subprocess(f'cp "{path_to_generaor_network}" {base_folder}/checkpoints/pix2pix/')
+    _run_subprocess(f'cp "{path_to_generaor_network}" {base_folder}/checkpoints/pix2pix/latest_net_G.pth')
+
+# This function evaluates the neural network on input image
+# Inputs:
+#   im - input image (input domain, e.g. OCT) in cv format (256x256x3). Input image should be masked and cropped.
+# Outputs:
+#   output image (in target domain, e.g. virtual histology) in cv format
+def run_network (im):
+    
+    # Input check
+    if im.shape[:2] != (256, 256):
+        raise ValueError("Image size must be 256x256 pixels to run model on.")
+
+    # Pix2Pix implementation expects 256x512 image. Pad with zeros
+    padded = np.zeros([256,512,3], np.uint8)
+    padded[:,:256,:] = im[:,:,:]
+    
+    # Write input image in the right folder structure
+    images_dir = f"{base_folder}/dataset/test"
+    os.makedirs(images_dir)
+    im_input_path = os.path.join(dir,"im1.jpg")
+    cv2.imwrite(im_input_path, padded)
+    
+    # Run pix2pix
+    _run_subprocess(f'{base_folder}/test.py --netG resnet_9blocks --dataroot "{base_folder}/dataset/"  --model pix2pix --name pix2pix --checkpoints_dir "{base_folder}/checkpoints" --results_dir "{base_folder}/results"
+
+    # Load output image
+    im_output_path = f"{base_folder}/results/pix2pix/test_latest/images/im1_fake_B.png"
+    im_output = cv2.imread(im_output_path)
+    
+    return(im_output)
+    
