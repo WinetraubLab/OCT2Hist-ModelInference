c76b807c79928e2542f1c5e2a0da85a23f23e2bf
MyYo <MyYo@users.noreply.github.com>
2023-08-22 19:24:46 -0700

    Refactor, allow for user to define path to generator network

--- utils/pix2pix.py
+++ utils/pix2pix.py
@@ -9,9 +9,6 @@ from google.colab import drive
 # Base folder already contains pix2pix code from https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix/tree/9f8f61e5a375c2e01c5187d093ce9c2409f409b0
 base_folder = "/content/OCT2Hist-UseModel/pytorch-CycleGAN-and-pix2pix"
 
-# Folder containing the pre-trained model
-model_folder = "/content/drive/Shareddrives/Yolab - Current Projects/_Datasets/2020-11-10 10x Model (Paper V2)"
-
 # Private module to wrapp around using cmd
 def _run_subprocess(cmd):
     result = subprocess.run([cmd], capture_output=True, text=True, shell=True)
@@ -22,8 +19,12 @@ def _run_subprocess(cmd):
         print(result.stderr)
         raise RuntimeError("See error")
 
-def setup_network():
-    """ Set up environment load the network parameters, run this code once """
+# Run this function to set up the Neural Network with Pre-trained generator network
+# path_to_generaor_network - path to generator network (*.pth file), default value is the OCT2Hist network
+# Set up environment load the network parameters, run this code once
+def setup_network(
+    path_to_generaor_network = "/content/drive/Shareddrives/Yolab - Current Projects/_Datasets/2020-11-10 10x Model (Paper V2)/latest_net_G.pth"
+    ):
     
     # Install dependencies
     _run_subprocess(f'pip install -r {base_folder}/requirements.txt')
@@ -34,5 +35,4 @@ def setup_network():
     # Copy model parameters to the correct location
     _run_subprocess(f'mkdir {base_folder}/checkpoints')
     _run_subprocess(f'mkdir {base_folder}/checkpoints/pix2pix/')
-    _run_subprocess(f'cp "{model_folder}/latest_net_G.pth" {base_folder}/checkpoints/pix2pix/')
-    _run_subprocess(f'cp "{model_folder}/latest_net_D.pth" {base_folder}/checkpoints/pix2pix/')
+    _run_subprocess(f'cp "{path_to_generaor_network}" {base_folder}/checkpoints/pix2pix/')
