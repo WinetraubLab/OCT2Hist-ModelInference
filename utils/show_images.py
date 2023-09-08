1c52adedc49f464fd38f230650765193aeca43cd
Danny Barash <57645588+dannybarash7@users.noreply.github.com>
2023-09-08 16:50:43 +0300

    Update show_images.py

--- utils/show_images.py
+++ utils/show_images.py
@@ -27,21 +27,21 @@ def showTwoImgs(img1, img2):
   plt.subplot(1,2,2);
   plt.imshow(img2);
 
-def showThreeImgs(image1, image2, image_to_mask, masks = None):
+def showThreeImgs(image1, image2, image_to_mask, masks = None, titles = []):
   # Create a figure with three subplots
   fig, axes = plt.subplots(1, 3, figsize=(15, 5))
-  
+  if titles
   # Display the first image in the first subplot
   axes[0].imshow(image1)
-  axes[0].set_title('Image 1')
+  
   
   # Display the second image in the second subplot
   axes[1].imshow(image2)
-  axes[1].set_title('Image 2')
+  
   
   # Display the third image in the third subplot
   axes[2].imshow(image_to_mask)
-  axes[2].set_title('Image 3')
+  
 
   if masks:
     if len(masks) == 0:
@@ -53,7 +53,10 @@ def showThreeImgs(image1, image2, image_to_mask, masks = None):
         color_mask = np.concatenate([np.random.random(3), [0.35]])
         img[m] = color_mask
     axes[2].imshow(img)
-
+  if titles:
+    axes[0].set_title(titles[0])
+    axes[1].set_title(titles[1])
+    axes[2].set_title(titles[2])
   
   # Adjust spacing between subplots
   plt.tight_layout()
