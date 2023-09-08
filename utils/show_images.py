dad176a4069d666511a025601349fc3c4de544c3
Danny Barash <57645588+dannybarash7@users.noreply.github.com>
2023-09-08 16:37:30 +0300

    Update show_images.py

--- utils/show_images.py
+++ utils/show_images.py
@@ -27,3 +27,24 @@ def showTwoImgs(img1, img2):
   plt.subplot(1,2,2);
   plt.imshow(img2);
 
+def showThreeImgs(image1, image2, image3):
+  # Create a figure with three subplots
+  fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+  
+  # Display the first image in the first subplot
+  axes[0].imshow(image1)  # Change 'gray' to the appropriate colormap
+  axes[0].set_title('Image 1')
+  
+  # Display the second image in the second subplot
+  axes[1].imshow(image2)  # Change 'gray' to the appropriate colormap
+  axes[1].set_title('Image 2')
+  
+  # Display the third image in the third subplot
+  axes[2].imshow(image3)  # Change 'gray' to the appropriate colormap
+  axes[2].set_title('Image 3')
+  
+  # Adjust spacing between subplots
+  plt.tight_layout()
+  
+  # Show the figure
+  plt.show()
