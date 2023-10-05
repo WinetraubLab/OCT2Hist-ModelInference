213457bbf73bac4a94ba61b85ad3fd25bde811a3
MyYo <MyYo@users.noreply.github.com>
2023-10-04 18:33:35 -0700

    Update crop.py to add padding in case size is too small

--- utils/crop.py
+++ utils/crop.py
@@ -1,12 +1,19 @@
-def crop(preprocessed_img, width, height, x0, z0):
-    #slice from image
-    # width = 256 * 4
-    # height = 256 * 2
-    # x0 = 135
-    # z0= 350
-    #check crop out of image
-    h,w,_ = preprocessed_img.shape
-    if z0>h or z0+height> h or x0>w or x0+width > w:
-        print("Did not crop, image is too small.")
-        return preprocessed_img
-    return preprocessed_img[z0:z0+height, x0:x0+width]
+import numpy as np
+
+# Crop part of the image, pad with nan if target size is bigger than input image
+def crop2(input_image, target_width=1024, target_height=512, x0=0, z0=0):
+    # Get the dimensions of the input image
+    input_height, input_width = input_image.shape[:2]
+
+    # Calculate the cropping region
+    x1 = min(input_width, x0 + target_width)
+    z1 = min(input_height, z0 + target_height)
+
+    # Crop the image
+    cropped_image = input_image[z0:z1, x0:x1]
+
+    # Add padding as needed to reach target width and height
+    pad_width = ((0, max(0, target_height - (z1 - z0))), (0, max(0, target_width - (x1 - x0))), (0, 0))
+    cropped_image_padded = np.pad(cropped_image, pad_width, mode='constant', constant_values=np.nan)
+
+    return cropped_image_padded
