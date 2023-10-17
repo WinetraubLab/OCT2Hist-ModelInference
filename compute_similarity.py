b16cc483ca8493d087030f7448e8e36067f77f16
MyYo <MyYo@users.noreply.github.com>
2023-10-16 20:00:50 -0700

    Allow external user to utilize blur image

--- compute_similarity.py
+++ compute_similarity.py
@@ -9,16 +9,6 @@ from skimage.metrics import structural_similarity as ssim
 # We can add blur radius for gaussian bluring (pixels)
 # Returns ssim
 def compute_similarity (im1, im2, blur_radius=0):
-  
-  def blur_image(image, blur_radius):
-    if blur_radius>0:
-        sigma = blur_radius
-        filter_size = int(2 * np.ceil(2 * sigma) + 1) # the default filter size in Matlab
-        filt_img = cv2.GaussianBlur(image, (filter_size, filter_size), sigma)
-        return filt_img
-    else:
-      return image
-
   # Function to calculate SSIM between two images
   def calculate_ssim(image1, image2):
       # Convert images to grayscale
@@ -35,3 +25,13 @@ def compute_similarity (im1, im2, blur_radius=0):
 
   # Compute simularity
   return calculate_ssim(blurred_im1,blurred_im2)
+
+# Blur image using gauissian filter
+def blur_image(image, blur_radius):
+  if blur_radius>0:
+      sigma = blur_radius
+      filter_size = int(2 * np.ceil(2 * sigma) + 1) # the default filter size in Matlab
+      filt_img = cv2.GaussianBlur(image, (filter_size, filter_size), sigma)
+      return filt_img
+  else:
+    return image
