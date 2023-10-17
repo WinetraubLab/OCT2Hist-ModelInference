40d0f4e47d14c97bdc3ed24ea08321e125c0ee1d
MyYo <MyYo@users.noreply.github.com>
2023-10-16 23:04:13 -0700

    Add more similarities

--- compute_similarity.py
+++ compute_similarity.py
@@ -4,27 +4,30 @@ import cv2
 import numpy as np
 from skimage import color
 from skimage.metrics import structural_similarity as ssim
+from sklearn.metrics import mean_squared_error as mse
 
 # Compute similarity between im1 and im2 (load using openCV).
 # We can add blur radius for gaussian bluring (pixels)
-# Returns ssim
+# Returns ssim, mse
 def compute_similarity (im1, im2, blur_radius=0):
   # Function to calculate SSIM between two images
-  def calculate_ssim(image1, image2):
+  def calculate_similarity(image1, image2):
       # Convert images to grayscale
       gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
       gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
   
-      # Compute SSIM
-      score, _ = ssim(gray1, gray2, full=True)
-      return score
+      # Compute different similarities
+      ssim, _ = ssim(gray1, gray2, full=True)
+      mse = mse(gray1, gray2, full=True)
+
+      return ssim, mse
     
   # Blur the images
   blurred_im1 = blur_image(im1, blur_radius)
   blurred_im2 = blur_image(im2, blur_radius)
 
   # Compute simularity
-  return calculate_ssim(blurred_im1,blurred_im2)
+  return calculate_ssim(blurred_im1,blurred_im2),
 
 # Blur image using gauissian filter
 def blur_image(image, blur_radius):
